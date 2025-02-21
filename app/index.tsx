5dd93e8c90d6806d92f4ecb9746c757f304976c4
WrestlerSquid <75701809+WrestlerSquid@users.noreply.github.com>
2025-02-20 23:24:33 -0500

    nativewind added

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 const index = () => {
   return (
-    <View>
-      <Text style={styles.container}>index</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>index</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Go</Link>
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
   )
 }
 
 export default index
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
