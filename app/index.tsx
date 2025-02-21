1f8ca4970d1be0a82ab689f53e9f5f8acf7a2d6d
WrestlerSquid <75701809+WrestlerSquid@users.noreply.github.com>
2025-02-21 00:15:50 -0500

    fontawesome

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
 
 const index = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Appraisal</Text>
+      <FontAwesome name="rocket" size={40} color="black" />
       <StatusBar style="auto" />
       <Link href="/profile">Go to Profile</Link>
     </View>
